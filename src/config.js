cc3b462628495e8f35dcfb26626961154f0fa8d6
kokonattuDream <kerjohn.chen@gmail.com>
2020-05-03 15:07:58 -0400

    signin

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const dev = {
+  backend_API: "localhost:3000"
+};
+
+module.exports = dev;
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,47 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+import config from '../config';
 
 Vue.use(Vuex);
 
+const errorSystem = {
+  state: {
+    show: false,
+    text: "Error"
+  },
+  mutations: {
+    showError(state, message) {
+      state.show = true;
+      state.text = message;
+    }
+  }
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    user: ""
+  },
+  getters: {
+    user: state => state.user
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
+    }
+  },
+  actions: {
+    async signIn(context){
+      try {
+        const user = (await axios.get(config.backend_API + "/user-session"))
+          .data;
+        context.commit("setUser", user);
+      } catch (error) {
+        context.commit("showError", error);
+      }
+    }
+  },
+  modules: {
+    error: errorSystem
+  }
 });
