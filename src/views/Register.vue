402dcd2dec45fd19c3b269dcecc74f1d108a0d09
kokonattuDream <kerjohn.chen@gmail.com>
2020-05-03 20:19:50 -0400

    register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,8 +10,7 @@
           <b-form-input
             id="username"
             v-model="form.username"
-            required
-            placeholder="Enter Username"
+            aria-describedby="input-1-live-feedback"
           ></b-form-input>
         </b-form-group>
 
@@ -25,20 +24,32 @@
             v-model="form.password"
             type="password"
             required
+            aria-describedby="input-2-live-feedback"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="password-input2"
+          label="Confirm Password:"
+          label-for="password"
+        >
+          <b-form-input
+            id="confirm_password"
+            v-model="form.confirm_password"
+            type="password"
+            required
             aria-describedby="password-help-block"
           ></b-form-input>
         </b-form-group>
+
         <div class="row justify-content-between">
           <div class="col-6">
-            <b-button type="submit" variant="primary">Submit</b-button>
+            <b-button type="submit" variant="primary">Register</b-button>
           </div>
           <div class="col-6">
-            <b-button type="reset" variant="secondary" @click="toRegister">
-              Register
-            </b-button>
+            <b-button type="reset" variant="secondary" @click="toLogIn">Login</b-button>
           </div>
         </div>
-
       </b-form>
     </div>
   </div>
@@ -51,21 +62,22 @@ export default {
     return {
       form: {
         username: "",
-        password: ""
+        password: "",
+        confirm_password: ""
       }
     };
   },
   methods: {
     async onSubmit(event) {
       event.preventDefault();
-      this.$store.dispatch("signIn", {
+      this.$store.dispatch("register", {
         username: this.username,
         password: this.password
       });
       //this.$router.push("Home");
     },
-    toRegister(){
-      this.$router.push("Register");
+    toLogIn(){
+      this.$router.push("Login");
     }
   }
 };
