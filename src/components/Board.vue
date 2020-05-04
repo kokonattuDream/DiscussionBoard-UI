7d3419c6fc89200bf52f566449ee673b14c946e7
kokonattuDream <kerjohn.chen@gmail.com>
2020-05-04 19:05:44 -0400

    post style

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
+    <div class="row justify-content-end">
+      <div class="col-sm-8" style="margin-bottom: 50px;">
+        <nav
+          class="navbar navbar-light"
+          style=" border-color: black; border-style: solid; background-color: #66ff99;"
+        >
+          Discussion Board
+        </nav>
+      </div>
+    </div>
+    <div class="row justify-content-end" style="margin-bottom: 20px;">
+      <div class="col-sm-2">
+        <b-button variant="outline-danger">Create New</b-button>
+      </div>
+    </div>
     <b-list-group v-for="post in posts" :key="`${post}`">
       <div class="row justify-content-end">
         <div class="col-sm-8">
-          <b-list-group-item>
-            {{ post.title }}
-          </b-list-group-item>
+          <b-card>
+            <b-card-title>
+              <a href="#" class="card-link">
+                {{ post.title }}
+              </a>
+            </b-card-title>
+            <b-card-text> By {{ post.user.username }} </b-card-text>
+            <template v-slot:footer>
+              <small class="text-muted">
+                {{ new Date(post.create_date).toLocaleString() }}
+              </small>
+            </template>
+          </b-card>
         </div>
       </div>
     </b-list-group>
