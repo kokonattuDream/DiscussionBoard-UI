be8044684dd92b354a5f4986b7ea036e00effa81
kokonattuDream <kerjohn.chen@gmail.com>
2020-05-17 10:15:09 -0400

    add side component render

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 let express = require('express');
-var serveStatic = require('serve-static');
+let serveStatic = require('serve-static');
+let  history = require('connect-history-api-fallback');
 
 let app = express();
+app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
-let port = process.env.PORT || 5000;
+let port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
